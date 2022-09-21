2937089b79d96c4c2362bdd9370462d56261f43c
k3vinrich4rd <kevinrichardve@gmail.com>
2022-09-21 00:32:40 -0300

    Inserindo implementações de testes feitos

--- src/test/java/testeAutomatizados/testeUnitariosAutomatizados/CalculoTest.java
+++ src/test/java/testeAutomatizados/testeUnitariosAutomatizados/CalculoTest.java
@@ -1,2 +1,43 @@
-package testeAutomatizados.testeUnitariosAutomatizados;public class CalculoTest {
+package testeAutomatizados.testeUnitariosAutomatizados;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class CalculoTest {
+
+    @Test
+    void TestarCalculoDeSoma(){
+        Calculo calculo = new Calculo();
+        float soma = calculo.soma(6, 7);
+        Assertions.assertEquals(13, soma);
+    }
+
+    @Test
+    void TestarAMensagemDeExcecaoDoMetodoDeRaizQuadrada(){
+        Calculo calculo = new Calculo();
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> calculo.raizQuadrada(-1));
+        Assertions.assertEquals("Erro, impossivel efetuar a raiz quadrada real para número negativos", exception.getMessage());
+    }
+
+    @Test
+    void TestarCalculoDeRaizQuadrada(){
+        Calculo calculo = new Calculo();
+        double raizQuadrada = calculo.raizQuadrada(25);
+        Assertions.assertEquals(5, raizQuadrada);
+    }
+
+    @Test
+    void TestandoRetornoDeUmNumeroImpar(){
+        Calculo calculo = new Calculo();
+        boolean impar = calculo.imparOuPar(3);
+        Assertions.assertTrue(impar);
+    }
+
+    @Test
+    void TestandoRetornoDeUmNumeroPar(){
+        Calculo calculo = new Calculo();
+        boolean par = calculo.imparOuPar(2);
+        Assertions.assertFalse(par);
+    }
 }
